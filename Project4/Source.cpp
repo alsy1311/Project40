43c1cb274fbd5d18654eda82177a509b3c02d380
alsy1311 <alsy1311@yandex.ru>
2020-03-28 17:07:57 +0300

    .

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-void main(){
+void getLine(int n, int m){
 	std::string c;
 	std::fstream in;
 	in.open("Source1.txt");
-	in.seekg(3*5, std::ios::beg);
+	in.seekg(n*m, std::ios::beg);
 	std::getline(in, c);
 	std::cout << c ;
 	in.close();
+
+}
+int main() {
+	getLine(5, 3);
 }
